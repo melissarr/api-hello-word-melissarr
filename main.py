b1728baf97e8c18cf50318fda19ccb0b05f938e5
melissarr <86131295+melissarr@users.noreply.github.com>
2021-09-03 00:21:53 -0300

    api-hello-word-melissarr

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+p= 0
+ i= 0
+    valor= int(input("Digite um valor:"))
+    if valor % 2 == 0
+        p= valorelse:
+        i= valor
+    print(f'Ovalor de p é: {p}')
+    print(f'Ovalor de i é: {i}')
     
+    return jsonify({'resultado':resultado})
+if__name__=="__main__":
+    app.run(debug=True)
