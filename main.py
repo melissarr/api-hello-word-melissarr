e62e1bc022b4d7d230a4b09ab6b150cc3750ee89
melissarr <86131295+melissarr@users.noreply.github.com>
2021-09-03 10:45:01 -0300

    m

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
+from flask import flask, jsonify, request
+app = flask(__name__)
 
 @app.route("/")
 def hello_world():
@@ -8,15 +7,15 @@ def hello_world():
     numero_2 = request.args.get('numero2', None)
     operacao = request.args.get('operacao', None)
     if not numero_1 or not numero_2 or not operacao:
-    return jsonify({
+        return jsonify({
       'mensagem': "Envie dois numeros, e se desejar multiplicar ou dividir.",
       'ajuda': "?numero1=X&numero2=X&operacao=X",
       'operacoes': 'multiplicar''dividir'
     })
     else:
-    if operacao == 'multiplicar':
-      return jsonify({
-        'resultado': int(numero_1) * int(numero_2)
+        if operacao == 'multiplicar':
+            return jsonify({
+                'resultado': int(numero_1) * int(numero_2)
       })
     if operacao == 'dividir':
       return jsonify({
@@ -27,5 +26,5 @@ def hello_world():
         'erro': 'operacao nao encontrada',
       })
 
-if __name__ == "__main":
-app.run(debug=True)
+    if __name__ == "__main__":
+        app.run()
