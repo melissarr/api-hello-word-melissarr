86c9ffac40acac5b1b47539dccc120ce4e41dd00
melissarr <86131295+melissarr@users.noreply.github.com>
2021-09-03 10:17:14 -0300

    m

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ def hello_world():
     numero_1 = request.args.get('numero1', None)
     numero_2 = request.args.get('numero2', None)
     operacao = request.args.get('operacao', None)
+    if not numero_1 or not numero_2 or not operacao:
+    return jsonify({
+      'mensagem': "Envie dois numeros, e se desejar multiplicar ou dividir.",
+      'ajuda': "?numero1=X&numero2=X&operacao=X",
+      'operacoes': 'multiplicar''dividir'
+    })
+    else:
+    if operacao == 'multiplicar':
+      return jsonify({
+        'resultado': int(numero_1) * int(numero_2)
+      })
     if operacao == 'dividir':
       return jsonify({
         'resultado': int(numero_1) / int(numero_2)
       })
-    else operacao == 'multiplicar':
-      return jsonify({
-        'resultado': int(numero_1) * int(numero_2)
-      })
     else:
-        resultado = "enive dois numeros e se deseja dividir ou multiplicar."
-    return jsonify({'resultado': resultado})
+      return jsonify({
+        'erro': 'operacao nao encontrada',
+      })
 
 if __name__ == "__main":
 app.run(debug=True)
